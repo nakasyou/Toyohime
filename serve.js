15c1281c2bcb47c4d7d58cc32b1e824fe9eb6916
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-13 21:13:04 +0900

    change to cjs.

--- serve.js
+++ serve.js
@@ -1,4 +1,4 @@
-import tor from "./tor/index.js";
+const tor = require("./tor/index.js");
 
 tor("https://https://api.ipify.org")
   .then(res=>res.text())
--- tor/index.js
+++ tor/index.js
@@ -1,6 +1,6 @@
 import tr from 'tor-request';
 
-export default (url) => new Promise((resolve, reject)=>{
+module.exports = (url) => new Promise((resolve, reject)=>{
   tr.request(url, (err, res, body) => {
     if(err){
       reject(err);
