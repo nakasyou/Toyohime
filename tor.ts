3e219fd6eb187faa1d5cf46acf33eca7e113fb0b
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-13 20:36:03 +0900

    fix

--- tor.ts
+++ tor.ts
@@ -1,5 +1,5 @@
 import tr from 'npm:tor-request';
-
+console.log("Your not tor ip is "+await(await fetch("https://api.ipify.org")).text())
 tr.request('https://api.ipify.org', (err, res, body)=>{
   if (!err && res.statusCode == 200) {
     console.log("Your public (through Tor) IP is: " + body);
