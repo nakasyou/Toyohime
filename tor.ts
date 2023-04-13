60d0e9a572952d63f7dc6224554596fd4a9474af
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-13 20:42:35 +0900

    fix

--- tor.ts
+++ tor.ts
@@ -1,4 +1,6 @@
 import tr from 'npm:tor-request';
+
+tr.setTorAddress("127.0.0.1",9050)
 console.log("Your not tor ip is "+await(await fetch("https://api.ipify.org")).text())
 tr.request('https://api.ipify.org', (err, res, body)=>{
   if (!err && res.statusCode == 200) {
