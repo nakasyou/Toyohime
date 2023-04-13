115a02f2fa04b4ca1dc6bfe96cfd99d4540726a2
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-13 21:06:33 +0900

    add reject return

--- tor/index.js
+++ tor/index.js
@@ -4,6 +4,7 @@ export default (url) => new Promise((resolve, reject)=>{
   tr.request(url, (err, res, body) => {
     if(err){
       reject(err);
+      return;
     }
     const response = new Response(body, {
       status: res.statusCode,
